174d7e99cac94b1d3944926c19f45d646d569549
Gonzalo Cámera <gonzalo.camera@rappi.com>
2019-02-05 15:57:02 -0300

    first commit 2

--- src/main/java/hello/ByeController.java
+++ src/main/java/hello/ByeController.java
@@ -2,9 +2,20 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ByeController {
+    private static RestTemplate restTemplate;
+
+    @Autowired
+    public ByeController(final RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
     @RequestMapping("/aa")
     public String index() {
@@ -15,7 +26,7 @@ public class ByeController {
     {
         final String uri = "https://api.github.com/users?since=135";
 
-        RestTemplate restTemplate = new RestTemplate();
+        restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
 
         System.out.println(result);
